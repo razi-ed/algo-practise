b9ca0a9ea0b801ed4f0f135975554d132c685cbf
razi.ed <razi.94@outlook.com>
2018-11-08 22:35:36 +0530

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,37 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// not so simplistic
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// my own
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reduce( ( acc, ele, i, arr) => {
+//       acc[ ( arr.length - 1 ) - i ] = ele;
+//       return acc;
+//     },[])
+//     .join('')
+// }
+
+// fancy
+const reverse = (str) => str
+    .split('')
+    .reduce( ( acc, ele ) => acc = ele + acc , '' )
+
+//more logical, simple and efficient
+// function reverse(str) {
+//   let reversed = '';
+//   for (const iterator of str) {
+//     reversed = iterator + reversed;
+//   }
+//   return reversed;
+// }
 
 module.exports = reverse;
