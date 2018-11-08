12ed65e6d75d7480c7b4d06001cf3ab7d97a899e
razi.ed <razi.94@outlook.com>
2018-11-08 23:13:20 +0530

    pallindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,37 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// USE When stripping unnecessary chars from string is need
+// function palindrome(str) {
+//   return !str
+//     .replace(/^[_a-zA-Z0-9]/g, '')
+//     .split( '' )
+//     .some( ( ele, i, arr) => arr[ ( arr.length -1 ) - i ] !== ele )
+// }
+
+// performs well in NOT Palindrome cases
+// function palindrome(str) {
+//   return !str
+//     // .replace(/^[_a-zA-Z0-9]/g, '')
+//     .split( '' )
+//     .some( ( ele, i, arr) => arr[ ( arr.length -1 ) - i ] !== ele )
+// }
+
+//logically better, atleast for me
+//if needed, split str into 2, if odd omit middle char, iterate through one half and check
+function palindrome(str) {
+  let iteretee;
+  if ( str.length % 2 === 0 ) {
+    iteretee = str.substring( ( str.length - 1 ) / 2 ).split('');
+  } else {
+    iteretee = str.substring( ( str.length - 2 ) / 2 ).split('');
+  }
+  return !iteretee
+    .some( ( ele, i, arr) => arr[ ( arr.length - 1) - i] !== str[ i ] )
+}
+
+// const palindrome = ( str ) => {
+//   return str.split('').reverse().join('') === str
+// }
 
 module.exports = palindrome;
